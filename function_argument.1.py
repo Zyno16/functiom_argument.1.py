29fff3271343d533452df2b06200ae0d43651f07
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-04 18:09:01 +0000

    Add files via upload

---
+++ function_argument.1.py
@@ -0,0 +1,13 @@
+def greet (lang):
+    if lang == "es":
+        return "hola"
+    elif lang == "eng":
+        return "hello"
+    elif lang == "fr":
+         return "bonjour" 
+
+print(greet("es"),"yusra")
+print(greet("fr"),"ines")
+print(greet("es"),"zino")
+
+    
